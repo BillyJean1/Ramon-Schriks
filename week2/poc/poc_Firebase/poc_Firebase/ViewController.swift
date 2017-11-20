51c4d84001610e8f47440415e3b0460dfda3f511
Ramon Schriks <ramon@xel.nl>
2017-11-20 12:22:02 +0100

    Added Tech 1 for week2, ek 1, firebase auth POC

--- week2/poc/poc_Firebase/poc_Firebase/ViewController.swift
+++ week2/poc/poc_Firebase/poc_Firebase/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userEmailField: UITextField!
+    @IBOutlet weak var userPasswordField: UITextField!
+    @IBOutlet weak var errorMessageField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        errorMessageField.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func showErrorMessage(msg: String) {
+        userEmailField.text = ""
+        userPasswordField.text = ""
+        
+        errorMessageField.isHidden = false
+        errorMessageField.text = msg
     }
+    
+    private func checkValidCredentials(email: String, password: String) -> Bool {
+        if !email.contains("@") || !email.contains(".") {
+            return false
+        }
+        return email.count >= 8 && password.count >= 8
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = userEmailField.text,
+            let password = userPasswordField.text {
+            
+            if (!checkValidCredentials(email: email, password: password)) {
+                showErrorMessage(msg: "Invalid input for email or password")
+                return
+            }
+                
+            Auth.auth().signIn(withEmail: email, password: password, completion: { user, error in
+                if error != nil {
+                    self.showErrorMessage(msg: "Login attempt failed: Incorrect credentials")
+                    return
+                }
+                
+                self.proceedLogin()
+                print("success login!")
+            })
+        }
+    }
+    
+    @IBAction func createAccountPressed(_ sender: UIButton) {
+       if let email = userEmailField.text,
+            let password = userPasswordField.text {
+        
+            if (!checkValidCredentials(email: email, password: password)) {
+                showErrorMessage(msg: "Invalid input for email or password")
+                return
+            }
+            
+            Auth.auth().createUser(withEmail: email, password: password, completion: { user, error in
+                if error != nil {
+                    self.showErrorMessage(msg: "Cannot create account: Already exists")
+                    return
+                }
+                
+                self.errorMessageField.isHidden = true
+                print("success created!")
+            })
+        }
+    }
+    
+    private func proceedLogin() {
+        self.errorMessageField.isHidden = true
+        self.userPasswordField.text = ""
 
-
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let loggedInViewController = storyboard.instantiateViewController(withIdentifier: "LoggedInViewController") as! LoggedInViewController
+        self.present(loggedInViewController, animated: true, completion: nil)
+    }
 }
 
